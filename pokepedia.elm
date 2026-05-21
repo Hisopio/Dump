a9ea6a2150eb12c61f29dd1514cae0219b2c1b14
Hisopio <mjason.discutido@gmail.com>
2026-05-22 07:28:53 +0800

    Add files via upload

--- pokepedia.elm
+++ pokepedia.elm
@@ -1,73 +1,315 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, text, input)
-import Html.Attributes exposing (placeholder)
-import Html.Events exposing (onClick, onInput)
+import Dict exposing (Dict)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onCheck, onInput)
+import Http
+import Json.Decode as Decode exposing (Decoder)
+import Set exposing (Set)
+
+-- TYPES
+
+type alias PokemonSummary =
+    { name : String
+    , id : String
+    }
+
+type alias PokemonData =
+    { name : String
+    , types : List String
+    , height : Int
+    , weight : Int
+    , sprite : String
+    }
 
 type alias Model =
-    { temperature : String, mode : Mode }
+    { query : String
+    , activeGenerations : Set Int
+    , generationCache : Dict Int (List PokemonSummary)
+    , pokemonCache : Dict String PokemonData
+    , results : List PokemonData
+    , loading : Bool
+    , pendingFetches : Int
+    }
 
 initialModel : Model
 initialModel =
-    { temperature = ""
-    , mode = CelciusToFahrenheit
+    { query = ""
+    , activeGenerations = Set.fromList [ 1 ]
+    , generationCache = Dict.empty
+    , pokemonCache = Dict.empty
+    , results = []
+    , loading = False
+    , pendingFetches = 0
     }
 
-type Mode
-    = CelciusToFahrenheit
-    | FahrenheitToCelcius
-
 type Msg
-    = Temperature String
-    | Switch
+    = UpdateQuery String
+    | ToggleGeneration Int Bool
+    | GotGeneration Int (Result Http.Error (List PokemonSummary))
+    | GotPokemon String (Result Http.Error PokemonData)
 
-modeLabel : Mode -> String
-modeLabel mode =
-    case mode of
-        CelciusToFahrenheit -> "Celsius to Fahrenheit"
-        FahrenheitToCelcius -> "Fahrenheit to Celsius"
+-- DECODERS
 
-update : Msg -> Model -> Model
+summaryListDecoder : Decoder (List PokemonSummary)
+summaryListDecoder =
+    Decode.field "pokemon_species"
+        (Decode.list
+            (Decode.map2 PokemonSummary
+                (Decode.field "name" Decode.string)
+                (Decode.at [ "url" ] Decode.string
+                    |> Decode.map extractId
+                )
+            )
+        )
+
+extractId : String -> String
+extractId url =
+    url
+        |> String.split "/"
+        |> List.filter (not << String.isEmpty)
+        |> List.reverse
+        |> List.head
+        |> Maybe.withDefault ""
+
+pokemonDecoder : Decoder PokemonData
+pokemonDecoder =
+    Decode.map5 PokemonData
+        (Decode.field "name" Decode.string)
+        (Decode.field "types"
+            (Decode.list (Decode.at [ "type", "name" ] Decode.string))
+        )
+        (Decode.field "height" Decode.int)
+        (Decode.field "weight" Decode.int)
+        (Decode.at [ "sprites", "front_default" ] Decode.string)
+
+-- UPDATE
+
+baseUrl : String
+baseUrl =
+    "https://pokeapi.co/api/v2"
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Temperature temp ->
-            { model | temperature = temp }
+        UpdateQuery q ->
+            let
+                newModel =
+                    { model | query = q, results = [], loading = True }
+            in
+            ( newModel, fetchAllActiveGenerations newModel )
 
-        Switch ->
-            { model | mode =
-                if model.mode == CelciusToFahrenheit then
-                    FahrenheitToCelcius
-                else
-                    CelciusToFahrenheit
-            }
+        ToggleGeneration gen checked ->
+            let
+                newGens =
+                    if checked then
+                        Set.insert gen model.activeGenerations
+                    else
+                        Set.remove gen model.activeGenerations
 
-convert : Mode -> String -> String
-convert mode temp =
-    case String.toFloat temp of
-        Nothing -> "Invalid input"
-        Just t ->
-            case mode of
-                CelciusToFahrenheit ->
-                    String.fromFloat (t * 9 / 5 + 32) ++ " °F"
-                FahrenheitToCelcius ->
-                    String.fromFloat ((t - 32) * 5 / 9) ++ " °C"
+                newModel =
+                    { model
+                        | activeGenerations = newGens
+                        , results = []
+                        , loading = True
+                    }
+            in
+            ( newModel, fetchAllActiveGenerations newModel )
+
+        GotGeneration genNum (Ok summaries) ->
+            let
+                updatedCache =
+                    Dict.insert genNum summaries model.generationCache
+
+                modelWithCache =
+                    { model | generationCache = updatedCache }
+
+                query =
+                    String.toLower model.query
+
+                matching =
+                    if String.isEmpty query then
+                        []
+                    else
+                        List.filter
+                            (\s -> String.startsWith query s.name)
+                            summaries
+                            
+                ( cachedResults, fetchCmds ) =
+                    List.foldl
+                        (\summary ( acc, cmds ) ->
+                            case Dict.get summary.name model.pokemonCache of
+                                Just cached ->
+                                    ( acc ++ [ cached ], cmds )
+
+                                Nothing ->
+                                    ( acc
+                                    , cmds
+                                        ++ [ Http.get
+                                                { url = baseUrl ++ "/pokemon/" ++ summary.id
+                                                , expect = Http.expectJson (GotPokemon summary.name) pokemonDecoder
+                                                }
+                                           ]
+                                    )
+                        )
+                        ( [], [] )
+                        matching
+
+                newPending =
+                    model.pendingFetches - 1 + List.length fetchCmds
+
+                newResults =
+                    model.results ++ cachedResults
+
+                stillLoading =
+                    newPending > 0
+            in
+            ( { modelWithCache
+                | results = newResults
+                , pendingFetches = newPending
+                , loading = stillLoading
+              }
+            , Cmd.batch fetchCmds
+            )
+
+        GotGeneration _ (Err _) ->
+            ( { model
+                | pendingFetches = model.pendingFetches - 1
+                , loading = model.pendingFetches - 1 > 0
+              }
+            , Cmd.none
+            )
+
+        GotPokemon speciesName (Ok pokemon) ->
+            let
+                newCache =
+                    Dict.insert speciesName pokemon model.pokemonCache
+
+                newPending =
+                    model.pendingFetches - 1
+            in
+            ( { model
+                | pokemonCache = newCache
+                , results = model.results ++ [ pokemon ]
+                , pendingFetches = newPending
+                , loading = newPending > 0
+              }
+            , Cmd.none
+            )
+
+        GotPokemon _ (Err _) ->
+            let
+                newPending =
+                    model.pendingFetches - 1
+            in
+            ( { model
+                | pendingFetches = newPending
+                , loading = newPending > 0
+              }
+            , Cmd.none
+            )
+
+
+fetchAllActiveGenerations : Model -> Cmd Msg
+fetchAllActiveGenerations model =
+    if String.isEmpty (String.toLower model.query) then
+        Cmd.none
+    else
+        model.activeGenerations
+            |> Set.toList
+            |> List.map
+                (\gen ->
+                    case Dict.get gen model.generationCache of
+                        Just _ ->
+                            -- Re-trigger GotGeneration with cached data
+                            Http.get
+                                { url = baseUrl ++ "/generation/" ++ String.fromInt gen
+                                , expect = Http.expectJson (GotGeneration gen) summaryListDecoder
+                                }
+
+                        Nothing ->
+                            Http.get
+                                { url = baseUrl ++ "/generation/" ++ String.fromInt gen
+                                , expect = Http.expectJson (GotGeneration gen) summaryListDecoder
+                                }
+                )
+            |> Cmd.batch
+
+-- VIEW
+
+
+viewTypeTag : String -> Html Msg
+viewTypeTag t =
+    span
+        [ 
+        ]
+        [ text t ]
+
+viewCard : PokemonData -> Html Msg
+viewCard pokemon =
+    div
+        [] [ img
+            [ src pokemon.sprite
+            , alt pokemon.name
+            ] []
+        , p [] [ text pokemon.name ]
+        , div [] (List.map viewTypeTag pokemon.types)
+        , div []
+            [ span [] [ text (String.fromFloat (toFloat pokemon.height / 10) ++ " m") ]
+            , span [] [ text "·" ]
+            , span [] [ text (String.fromFloat (toFloat pokemon.weight / 10) ++ " kg") ]
+            ]
+        ]
+
+viewCheckbox : Set Int -> Int -> Html Msg
+viewCheckbox active gen =
+    label
+        [] [ input
+            [ type_ "checkbox"
+            , checked (Set.member gen active)
+            , onCheck (ToggleGeneration gen)
+            ] []
+        , text ("Gen " ++ String.fromInt gen)
+        ]
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ button [ onClick Switch ] [ text ("Convert from: " ++ modeLabel model.mode) ]
-        , div [] []
-        , input [ onInput Temperature, placeholder "Enter temperature" ] []
-        , div [] [ text (convert model.mode model.temperature) ]
+    div
+        [] [ div [] [ h1 [] [ text "Pokédex" ]
+        , input
+            [ type_ "text"
+            , placeholder "Search Pokémon by name..."
+            , value model.query
+            , onInput UpdateQuery
+            ] []
+        , div []
+            (List.map (viewCheckbox model.activeGenerations) (List.range 1 9))
+        , if String.isEmpty model.query then
+            p [] [ text "Type a Pokémon name to begin searching." ]
+        else if model.loading && List.isEmpty model.results then
+            p [] [ text "Loading..." ]
+        else if not model.loading && List.isEmpty model.results then
+            p [] [ text "No Pokémon found." ]
+        else
+            div []
+                [ if model.loading then
+                    p [] [ text "Loading more..." ]
+                        else
+                    text ""
+                , div [] (List.map viewCard model.results)
+                ]
+            ]
         ]
+
+-- MAIN
 
 main : Program () Model Msg
 main =
-    Browser.sandbox
-        { init = initialModel
+    Browser.element
+        { init = \_ -> ( initialModel, Cmd.none )
         , view = view
         , update = update
+        , subscriptions = \_ -> Sub.none
         }
-
-
--- pseudoflames.elm
+++ pseudoflames.elm
@@ -1,77 +1,51 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, text, p, input)
+import Html exposing (Html, button, div, text, input)
 import Html.Events exposing (onClick, onInput)
-import Html.Attributes exposing (value, placeholder)
+import Html.Attributes exposing (type_, placeholder, value)
 
 type alias Model =
-    { noteArr : List String
-    , currNote : String
+    { texta : String
+    , textb : String
     }
 
 initialModel : Model
 initialModel =
-    { noteArr = []
-    , currNote = ""
+    { texta = ""
+    , textb = ""
     }
 
 type Msg
-    = Submit
-    | RemoveNote Int
-    | UpdateCurrNote String
+    = TextA String
+    | TextB String
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        UpdateCurrNote str ->
-            { model | currNote = str }
+        TextA str ->
+            { model | texta = str }
 
-        Submit ->
-            if String.isEmpty (String.trim model.currNote) then
-                model
-            else
-                { model
-                    | noteArr = model.noteArr ++ [ model.currNote ]
-                    , currNote = ""
-                }
-
-        RemoveNote idx ->
-            { model | noteArr = List.take idx model.noteArr ++ List.drop (idx + 1) model.noteArr }
+        TextB str ->
+            { model | textb = str }
 
 view : Model -> Html Msg
 view model =
     let
-        noteItems : List (Html Msg)
-        noteItems =
-            List.indexedMap
-                (\idx note ->
-                    p []
-                        [ text (note ++ "   ")
-                        , button [ onClick (RemoveNote idx) ] [ text "Remove Note" ]
-                        ]
-                )
-                model.noteArr
+        result =
+            case ( String.toInt model.texta, String.toInt model.textb ) of
+                ( Just a, Just b ) ->
+                    div [] [ text ("Sum is: " ++ String.fromInt (a + b)) ]
 
-        emptyMsg : List (Html Msg)
-        emptyMsg =
-            if List.isEmpty model.noteArr then
-                [ p [] [ text "No notes yet!" ] ]
-            else
-                []
+                _ ->
+                    div [] [ text "Invalid input" ]
     in
     div []
-        (emptyMsg
-            ++ noteItems
-            ++ [ input
-                    [ onInput UpdateCurrNote
-                    , value model.currNote
-                    , placeholder "Enter a note..."
-                    ]
-                    []
-               , button [ onClick Submit ] [ text "Add Note" ]
-               ]
-        )
+        [ input [ placeholder "First number",  onInput TextA, value model.texta ] []
+        , div [] []
+        , input [ placeholder "Second number", onInput TextB, value model.textb ] []
+        , result
+        ]
 
 main : Program () Model Msg
 main =
